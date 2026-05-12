7fa69a710983f1fca4cd23e255fac0bdcf7d530e
harshverma2546 <200617harsh@gmail.com>
2026-05-12 14:37:13 +0530

    code

---
+++ javascript/index.js
@@ -0,0 +1,38 @@
+let mixedobject={
+    name:"alice",
+    Age:25,
+    hobbies:["reading","travelling","coding"]
+    console.log(mixedobject);
+    console.log(mixedobject);
+}
+console.log(mixedobject.name);
+console.log(mixedobject.age);
+console.log(mixedobject.hobbies);
+console.log(mixedobject.hobbies[0]);
+console.log(mixedobject.hobbies[1]);
+console.log(mixedobject.hobbies[2]);
+console.log(mixedobject.address);
+
+console.log(mixedobject.adddress.street);
+console.log(mixedobject.address.city);
+console.log(mixedobject.adddress.country);
+
+mixedobject.address.greet();
+//object inside array
+let [{name:"alice",age:25}
+    {name:"bob",age:25}
+    {name:"charlie",age:25}]
+    console.log(people[0]);
+    console.log(people[1]);
+    console.log(people[2]);
+//array inside object
+let emp={
+    //create a single employee object from the input fields
+    let emp={
+        name:document.getElementById("name").ariaValueMax,
+        id;document.getElementaryBy
+    }
+}    
+
+
+
